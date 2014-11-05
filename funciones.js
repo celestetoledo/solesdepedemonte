a721c8e1607356ef3199399bd2d4ea39c1406c18
Amira <amira.s@hotmail.com.ar>
2014-11-05 12:59:22 -0300

    java para menu-little

---
+++ funciones.js
@@ -0,0 +1,10 @@
+$('.menu-little').hide();
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+        $('.menu-little').show();
+    } else {
+        $('.menu-little').hide();
+    }
+});
