b0aa22f22268b4428ce429b499f5b65004fe7952
Amira <amira.s@hotmail.com.ar>
2014-11-07 22:04:47 -0300

    sumando css
    agregando class para darle estilo a Inicio

--- funciones.js
+++ funciones.js
@@ -2,7 +2,7 @@ $('.menu-little').hide();
 
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
-    if (scroll > 500) {
+    if (scroll > 550) {
         $('.menu-little').show();
     } else {
         $('.menu-little').hide();
