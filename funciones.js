522a94a7daa3c2f47bb6333de03727d23af269da
Alejandro Prieto <plaxtilino@gmail.com>
2014-11-30 20:27:32 -0300

    Merge

--- funciones.js
+++ funciones.js
@@ -2,7 +2,11 @@ $('.containerlittle').hide();
 
 $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
+<<<<<<< Updated upstream
     if (scroll > 450) {
+=======
+    if (scroll > 500) {
+>>>>>>> Stashed changes
         $('.containerlittle').show();
     } else {
         $('.containerlittle').hide();
